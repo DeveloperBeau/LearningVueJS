08811798f888da184241c206804dd39216cea4ea
Beau <beau@domesticcat.com.au>
2018-05-12 16:06:41 +1000

    Adding code so far

---
+++ vuejs-complete/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<app-servers></app-servers>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      status: 'Critical'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
